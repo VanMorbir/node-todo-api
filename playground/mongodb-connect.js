92aeab05f91f78a8b4711823d4975844f6fcb4f9
VanMorbir <vanmorbir@gmail.com>
2017-10-31 12:10:41 +0200

    Init commit

---
+++ playground/mongodb-connect.js
@@ -0,0 +1,44 @@
+//const MongoClient = require('mongodb').MongoClient;
+const {MongoClient, ObjectID} = require('mongodb');
+
+var obj = new ObjectID();
+console.log(obj);
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp',
+                   (err, db) =>
+{
+  if(err)
+  {
+    return console.log('unable to connect to MongoDB server');
+  }
+  console.log('Connected to MongoDB server');
+
+  // db.collection('Todos').insertOne(
+  // {
+  //   text: 'Something to do',
+  //   completed: false
+  // }, (err, result) =>
+  // {
+  //   if (err)
+  //   {
+  //     return console.log('Unable to insert todo');
+  //   }
+  //   console.log(JSON.stringify(result.ops, undefined, 2));
+  //});
+
+  // db.collection('Users').insertOne(
+  //   {
+  //     name: 'Gil',
+  //     age: 29,
+  //     location: 'Israel'
+  //   }, (err, result) =>
+  //   {
+  //     if (err)
+  //     {
+  //       return console.log('Unable to insert User', err);
+  //     }
+  //     console.log(JSON.stringify(result.ops, undefined, 2));
+  //   });
+
+  db.close();
+});
