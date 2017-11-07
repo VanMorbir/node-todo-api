65ebde58aaca9396c98f61ac1add667c7dc2bd12
VanMorbir <vanmorbir@gmail.com>
2017-11-07 13:06:34 +0200

    Add test for GET /todos

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,9 +4,22 @@ const request = require('supertest');
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
 
+const todosSeed =
+[
+  {
+    text: "First test todo"
+  },
+  {
+    text: "Second test todo"
+  }
+];
+
 beforeEach((done) =>
 {
-  Todo.remove({}).then(() => done());
+  Todo.remove({}).then(() =>
+  {
+    Todo.insertMany(todosSeed);
+  }).then(() => done());
 });
 
 describe('POST /todos', () =>
@@ -29,7 +42,7 @@ describe('POST /todos', () =>
       {
         return done(err);
       }
-      Todo.find().then((todos) =>
+      Todo.find({text}).then((todos) =>
       {
         expect(todos.length).toBe(1);
         expect(todos[0].text).toBe(text);
@@ -52,9 +65,24 @@ describe('POST /todos', () =>
        }
        Todo.find().then((todos) =>
        {
-         expect(todos.length).toBe(0);
+         expect(todos.length).toBe(2);
          done();
        }).catch((e) => done(e));
      });
+  });
+});
+
+describe('GET /todos', () =>
+{
+  it('should get all todos', (done) =>
+  {
+    request(app)
+    .get('/todos')
+    .expect(200)
+    .expect((res) =>
+    {
+      expect(res.body.todos.length).toBe(2);
+    })
+    .end(done);
   });
 });
